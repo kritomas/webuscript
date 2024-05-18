5c890bdbab62b9c0c575bfae753765d2814a8fae
kritomas <Unknown>
2024-05-18 18:19:58 +0200

    Added login and register to styles.css.

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
 	<head>
 		<meta charset="utf-8">
-		<link rel="stylesheet" href="./style.css">
+		<link rel="stylesheet" href="./styles.css">
 		<title>Webuscript</title>
 	</head>
 	<body>
